a518d4a72e92bcb251588dd8793d384e882a95e4
ElijahCoding <cuiyeqing960904@gmail.com>
2020-12-27 23:45:35 +0300

    building signals

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .utils import get_random_code
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
@@ -16,3 +17,6 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}--{self.created}"
+
+    def save(self, *args, **kwargs):
+        pass
