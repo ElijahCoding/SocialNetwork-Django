94e724bf3ce9366c919ed635b55d408650df174b
ElijahCoding <cuiyeqing960904@gmail.com>
2021-01-13 12:47:03 +0300

    building profile templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Profile
+from django.shortcuts import render
 
-# Create your views here.
+def my_profile_view(request):
+    obj = Profile.objects.get(user=request.user)
+
+    context = {
+        'obj': obj
+    }
+
+    return render(request, 'profiles/myprofile.html', context)
