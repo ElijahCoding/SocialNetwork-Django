2cf53b0b038d47490c4018680153373a7d02cac3
ElijahCoding <cuiyeqing960904@gmail.com>
2021-01-13 23:20:34 +0300

    form built

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Profile
 from django.shortcuts import render
+from .forms import ProfileModelForm
 
 def my_profile_view(request):
     obj = Profile.objects.get(user=request.user)
