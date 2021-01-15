a57dbfa4d3da84219f41259a57202f915fff6034
ElijahCoding <cuiyeqing960904@gmail.com>
2021-01-15 17:15:14 +0300

    post model built

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from profiles.models import Profile
 
-# Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])], blank=True)
+    liked = models.ManyToManyField(Profile, default=None, related_name='likes')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return str(self.content[:20])
